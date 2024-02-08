7daaae0aacbbadb3ad17100555aaa8cafd11b213
jesusrpd <jesusrodrigo881@gmail.com>
2024-02-07 21:01:45 -0600

    eliminación btn y p

--- app/page.js
+++ app/page.js
@@ -5,10 +5,7 @@ import {Button} from "@nextui-org/react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className='text-white'>Página de inicio</p>
-      <Button color="primary">
-      Button
-    </Button>
+      
     </main>
   )
 }
